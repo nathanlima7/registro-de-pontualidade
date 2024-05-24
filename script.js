3a5c08ca369396b018f05fac96fe0f949cf8aca0
Nathan Lima <126731584+nathanlima7@users.noreply.github.com>
2024-05-23 23:44:42 -0300

    script.js

--- script.js
+++ script.js
@@ -1,4 +1,32 @@
+//Atualiza o segundo seletor de acordo com a escolha do primeiro...
+document.addEventListener('DOMContentLoaded', () => {
+
+    const turnoSeletor = document.getElementById('turnos');
+    const turmaSeletor = document.getElementById('turmas');
+
+    const opcoesManha = ['      ', '5º E.F', '6º E.F', '7º E.F', '8º E.F'];
+    const opcoesTarde = ['      ', '1º E.M', '2º E.M', '3º E.M'];
+
+    function attTurma(){
+        turmaSeletor.innerHTML = '';
+
+        const opcoes = turnoSeletor.value === 'manha' ? opcoesManha : opcoesTarde;
+
+        opcoes.forEach(opcao => {
+            const opcaoElemento = document.createElement('option');
+            opcaoElemento.value = opcao.toLowerCase();
+            opcaoElemento.textContent = opcao;
+            turmaSeletor.appendChild(opcaoElemento);
+        });
+    }
+
+    attTurma();
+
+    turnoSeletor.addEventListener('change', attTurma);
+});
+
 document.addEventListener("DOMContentLoaded", function() {
+
     const students = [
         "Ana Silva", "Bruno Lima", "Carlos Oliveira", "Daniela Costa", "Eduardo Souza",
         "Fernanda Almeida", "Gabriel Ferreira", "Helena Martins", "Igor Rodrigues", "Juliana Santos",
@@ -43,7 +71,7 @@ const ano = hoje.getFullYear();
 
 const formatoData = `${dia}_${mes}_${ano}`;
 
-const nomeArquivo = `Registro_de_pontualidade 5_ano ${formatoData}.pdf`;
+const nomeArquivo = `Registro_de_pontualidade ${formatoData}.pdf`;
 
 function exportToPDF() {
     const element = document.querySelector('.exportar');
